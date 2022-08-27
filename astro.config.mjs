f1e88622423979d0eeee9ed447ff0ce08b09e153
Thomas Reggi <thomas@reggi.com>
2022-08-26 22:40:48 -0400

    with deno intergration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import deno from "@astrojs/deno";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: deno()
+});
